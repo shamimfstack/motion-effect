8d087e4ed5b19e005e8380ea6181395b44f8a04a
shamimfstack <shamimfstack@gmail.com>
2024-05-18 10:04:27 +0600

    navbar added

---
+++ src/Routes/router.jsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layout/MainLayout";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout />,
+        children: [
+            {
+                path: "/",
+                element: <Home ></Home>
+            }
+        ]
+    }
+])
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import siteLogo from "../assets/MotionArtEffect-logo.png";
+
+export default function Navbar() {
+  return (
+    <div className="pt-6">
+      <nav className="flex justify-between max-w-7xl mx-auto">
+        <div>
+          <a href="/">
+            <img src={siteLogo} alt="site logo" />
+          </a>
+        </div>
+        <div>
+          <button className="bg-white px-12 py-3 rounded-md border-2 border-white  hover:text-white hover:bg-transparent">
+            Purchase Now
+          </button>
+        </div>
+      </nav>
+    </div>
+  );
+}
